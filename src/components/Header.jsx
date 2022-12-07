3273a509363fd2f5660a40dfd218eb0d7a0e3e4c
Ben Sales <adebsa2401@gmail.com>
2022-12-07 03:41:08 +0100

    fix: Remove active class

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -8,7 +8,7 @@ export default class Header extends React.PureComponent {
       <header>
         <h1>Math Magicians</h1>
         <nav>
-          <Link to="/" className="active">Home</Link>
+          <Link to="/">Home</Link>
           <Link to="calculator">Calculator</Link>
           <Link to="quote">Quote</Link>
         </nav>
