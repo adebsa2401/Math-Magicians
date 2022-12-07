5d79757dd8782d9d3ab5a5e52fe814baae45f821
Ben Sales <adebsa2401@gmail.com>
2022-12-07 11:35:13 +0100

    test: Add unit test for App component

--- src/__test__/App.test.js
+++ src/__test__/App.test.js
@@ -1,6 +1,7 @@
-import renderer from ‘react-test-renderer’;
-import App from ‘../App’;
-it(‘The App component renders correctly’, () => {
+import renderer from 'react-test-renderer';
+import App from '../App';
+
+it('The App component renders correctly', () => {
   const tree = renderer.create(<App />).toJSON();
   expect(tree).toMatchSnapshot();
 });
