c2886f935cd40331b40287b0435a33b0294d021f
frehiywot nega <fr.nega@gmail.com>
2022-12-07 13:30:22 +0300

    added change to app.test.js file

---
+++ src/__test__/App.test.js
@@ -0,0 +1,6 @@
+import renderer from ‘react-test-renderer’;
+import App from ‘../App’;
+it(‘The App component renders correctly’, () => {
+  const tree = renderer.create(<App />).toJSON();
+  expect(tree).toMatchSnapshot();
+});
